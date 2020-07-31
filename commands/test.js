6c6320b2675e359b6d72a2b5fc1fe872aa4b11cf
PreventAskingWhy <69006847+PreventAskingWhy@users.noreply.github.com>
2020-07-31 23:00:54 +0800

    First

--- commands/test.js
+++ commands/test.js
@@ -5,6 +5,6 @@ module.exports = {
         const random = ["hi", "hello", "world"]
         const randomMessage = random[Math.floor(Math.random() * random.length)];
         console.log(randomMessage)
-        message.channel.send(`${randomMessage}`)
+        message.channel.send(randomMessage)
     }
 }
