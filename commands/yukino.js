d35bdfc3d45778f5184057a05fb82f42d536fa76
PreventAskingWhy <69006847+PreventAskingWhy@users.noreply.github.com>
2020-07-31 11:12:23 +0800

    First

--- commands/yukino.js
+++ commands/yukino.js
@@ -1,14 +1,15 @@
 const { description } = require("./tea");
 
-let replies = ["https://i.redd.it/myz0gso6o7d51.jpg", "https://cdn.discordapp.com/attachments/703844627383320586/722014446997864468/api.png", "https://cdn.discordapp.com/attachments/703844627383320586/722012598106718228/4d05bb09-c46d-4916-8c7f-0294c280ef15.png"];
-let random = Math.floor(Math.random() * 3);
+const messages = ["message one", "message two", "message three", "message four"]
+
+const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+
+console.log(randomMessage)
 
 module.exports = {
-    name: 'yukino',
-    description: "Praise Yukino!",
+    name: 'test',
+    description: "test",
     execute(message, args){
-        message.channel.send({
-            to: channelID,
-            message: replies[random]})
+        message.channel.send(randomMessage)
     }
 }
