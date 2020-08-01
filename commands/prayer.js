63469313419827e3f19431b3400b00a774938df1
PreventAskingWhy <69006847+PreventAskingWhy@users.noreply.github.com>
2020-08-01 11:43:58 +0800

    First

---
+++ commands/prayer.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'prayer',
+    description: "prayer",
+    execute(message, args){
+        message.channel.send('Our Senpai in heaven, \n hallowed be your smile.');
+    }
+}
