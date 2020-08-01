bc37fc7cd914331d70ed42dea53a5d803d2db424
PreventAskingWhy <69006847+PreventAskingWhy@users.noreply.github.com>
2020-08-01 11:48:32 +0800

    First

--- commands/prayer.js
+++ commands/prayer.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'prayer',
     description: "prayer",
     execute(message, args){
-        message.channel.send('Our Senpai in heaven, \n hallowed be your smile.');
+        message.channel.send('Our Senpai in heaven, \nhallowed be your smile.');
     }
 }
--- commands/salt.js
+++ commands/salt.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'salt',
     description: "salt",
     execute(message, args){
-        message.channel.send('nobody ever buys salt, its always just there by default, how does it get there no clue, ive never bought it, have you? ive had a whole lot of people in my crappy life, who abandoned and ditched me and left me with strife. but i know no matter how bad the assault, that somewhere nearby me therell probably be salt');
+        message.channel.send('nobody ever buys salt, \nits always just there by default. \nhow does it get there no clue, \nive never bought it, have you? \nive had a whole lot of people in my crappy life, \nwho abandoned and ditched me and left me with strife. \nbut i know no matter how bad the assault, \nthat somewhere nearby me therell probably be salt');
     }
 }
