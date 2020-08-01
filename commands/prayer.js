c18dafe57576a019b3b671165417adb636d5f21e
PreventAskingWhy <69006847+PreventAskingWhy@users.noreply.github.com>
2020-08-01 11:52:55 +0800

    First

--- commands/prayer.js
+++ commands/prayer.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'prayer',
-    description: "prayer",
+    description: "Pray to Senpai!",
     execute(message, args){
         message.channel.send('Our Senpai in heaven, \nhallowed be your smile.\nYour TV-show come,\nYour wish will be done.\nAs in 2D,\nSo in 3D.\nGive us today our daily joy.\nAnd forgive us our lewdness,\nAs we forgive those who favor another.\nLead us not into despair,\nBut deliver us from depression.\nFor the beauty, the cuteness and the elegance are yours\nNow and forever.\nAmen.');
     }
