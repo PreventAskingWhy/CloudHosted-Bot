bb2491b062e6ed799bad7e191ed115e9703f4246
PreventAskingWhy <69006847+PreventAskingWhy@users.noreply.github.com>
2020-08-01 11:59:09 +0800

    First

--- main.js
+++ main.js
@@ -43,6 +43,8 @@ client.on('message', message =>{
         client.commands.get('maisan').execute(message, args);
     }else if(command === 'prayer'){
         client.commands.get('prayer').execute(message, args);
+    }else if(command === 'goodwaifu'){
+        client.commands.get('goodwaifu').execute(message, args);
     }
 });
 
