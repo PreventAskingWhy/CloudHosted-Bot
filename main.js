40d9781f6bf652a30e22b5de805bd86503dd2eea
PreventAskingWhy <69006847+PreventAskingWhy@users.noreply.github.com>
2020-08-04 10:54:22 +0800

    First

--- main.js
+++ main.js
@@ -54,7 +54,6 @@ client.on('message', message =>{
 
 client.on("message", async message => {
     if(message.author.bot || message.channel.type === "dm") return;
-    let prefix = botsettings.prefix;
     let messageArray = message.content.split(" ")
     let cmd = messageArray[0];
 
