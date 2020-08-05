73c3e897e53bf16d77c791202d337c5b86e0defd
PreventAskingWhy <69006847+PreventAskingWhy@users.noreply.github.com>
2020-08-05 11:17:52 +0800

    First

--- commands/salt.js
+++ commands/salt.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'salt',
     description: "salt",
     execute(message, args){
-        message.channel.send('nobody ever buys salt, \nits always just there by default. \nhow does it get there no clue, \nive never bought it, have you? \nive had a whole lot of people in my crappy life, \nwho abandoned and ditched me and left me with strife. \nbut i know no matter how bad the assault, \nthat somewhere nearby me therell probably be salt');
+        message.channel.send(`nobody ever buys salt, \nits always just there by default. \nhow does it get there no clue, \nive never bought it, have you? \nive had a whole lot of people in my crappy life, \nwho abandoned and ditched me and left me with strife. \nbut i know no matter how bad the assault, \nthat somewhere nearby me there'll probably be salt`);
     }
 }
