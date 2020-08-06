4d77492c6c8147ddb71fe52fe101cb428a18e0a5
PreventAskingWhy <69006847+PreventAskingWhy@users.noreply.github.com>
2020-08-06 19:25:47 +0800

    First

--- commands/boop.js
+++ commands/boop.js
@@ -2,6 +2,9 @@ module.exports = {
     name:'boop',
     description: "b o o p",
     execute(message, args){
-        message.channel.send('https://tenor.com/view/anime-boop-nose-blush-manga-gif-14746195')
+        const random = ['https://tenor.com/view/anime-boop-nose-blush-manga-gif-14746195', 'https://tenor.com/view/boop-rascal-does-not-dream-of-bunny-girl-senpai-anime-touch-nose-poke-gif-17682170']
+        const randomMessage = random[Math.floor(Math.random() * random.length)];
+        console.log(randomMessage)
+        message.channel.send(randomMessage)
     }
 }
