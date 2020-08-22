43165644a75a0cf3e5cf15bdeaed1d97e4be031c
PreventAskingWhy <69006847+PreventAskingWhy@users.noreply.github.com>
2020-08-22 19:54:42 +0800

    First

---
+++ commands/help.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'help',
+    description: "list of commands",
+    execute(message, args){
+        message.channel.send('!boop\n!chizuru\n!goodwaifu\n!maisan\n!prayer\n!quotes\n!salt\n!tea\n!yukino');
+    }
+}
